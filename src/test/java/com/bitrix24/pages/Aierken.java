4b9af686559eeacde32e764cb3d90f2431d452f9
Aierken712 <ershad0419@gmail.com>
2020-04-09 22:05:45 -0400

    added Aierken class

---
+++ src/test/java/com/bitrix24/pages/Aierken.java
@@ -0,0 +1,10 @@
+package com.bitrix24.pages;
+
+import org.bson.io.BsonOutput;
+
+public class Aierken {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Selenium");
+    }
+}
