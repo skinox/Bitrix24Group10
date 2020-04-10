1d1597bceb40932b03693c4084b57211970e1040
Aersilan Aji <arsilan12@gmail.com>
2020-04-09 22:04:07 -0400

    updated

--- src/test/java/com/bitrix24/pages/Arsilan.java
+++ src/test/java/com/bitrix24/pages/Arsilan.java
@@ -3,5 +3,6 @@ package com.bitrix24.pages;
 public class Arsilan {
     public static void main(String[] args) {
         System.out.println("testing");
+        System.out.println("testing2!!!!");
     }
 }
