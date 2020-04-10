557334a21574ab61e8cdaffe9a9ad503dcdbe448
Aierken712 <ershad0419@gmail.com>
2020-04-10 19:33:17 -0400

    resolved conflict

--- src/test/java/com/bitrix24/pages/Pages.java
+++ src/test/java/com/bitrix24/pages/Pages.java
@@ -4,11 +4,8 @@ public class Pages {
 
 
     // Hello world
-<<<<<<< HEAD
     public static void main(String[] args) {
         System.out.println("Selenium");
     }
-=======
->>>>>>> Aierken
 
 }
