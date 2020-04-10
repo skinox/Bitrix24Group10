097d4be8e9e6a77a4f5d9f98d901b7ff364128a4
Manuchehr Mirzoev <m.m.manuchehri1@gmail.com>
2020-04-09 22:45:54 -0400

    main method is added

--- src/test/java/com/bitrix24/pages/demo.java
+++ src/test/java/com/bitrix24/pages/demo.java
@@ -2,4 +2,8 @@ package com.bitrix24.pages;
 
 public class demo {
     // try to push it to master
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+    }
 }
