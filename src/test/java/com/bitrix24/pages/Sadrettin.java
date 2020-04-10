1c35a2d8a4e203b76e7a58b06afacd5be8f054bd
sadrettincali <sadrettincali@gmail.com>
2020-04-09 22:11:38 -0400

    new commit

--- src/test/java/com/bitrix24/pages/Sadrettin.java
+++ src/test/java/com/bitrix24/pages/Sadrettin.java
@@ -3,5 +3,7 @@ package com.bitrix24.pages;
 public class Sadrettin {
     public static void main(String[] args) {
         System.out.println("testing to test...");
+        System.out.println("testing to test to testing...");
+
     }
 }
