0aa9960f99e221440d4c9f8558b6a1ff5b5cb070
Manuchehr Mirzoev <m.m.manuchehri1@gmail.com>
2020-04-11 01:57:32 -0400

    navigateTo, search and getCurrentUserEmail methods

--- src/test/java/com/bitrix24/pages/AbstractBasePage.java
+++ src/test/java/com/bitrix24/pages/AbstractBasePage.java
@@ -24,17 +24,29 @@ public AbstractBasePage(){
     PageFactory.initElements(Driver.getDriver(), this);
 }
 
+    /**
+     * this method help us to move into other modules like Drive, Mail and Tasks
+     * @param componentName
+     */
     public void navigateTo(String componentName){
         // all component name starts with capital letter like Drive, Mail
         String locator = "//span[contains(text(),'"+componentName+"')]";
         wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(locator))).click();
     }
 
+    /**
+     * this method helps us to search any people or document in the search engine
+     * @param anything
+     */
     public void search(String anything){
         wait.until(ExpectedConditions.presenceOfElementLocated(By.id("search-textbox-input"))).sendKeys(anything, Keys.ENTER);
 
     }
 
+    /**
+     * this method give us the email of current user as a string
+     * @return currentUserEmail
+     */
     public String getCurrentUserEmail(){
 
         wait.until(ExpectedConditions.visibilityOf(currentUserEmail));
