c7e89606fca1e5ba5d022d9279535ef0536b2a43
Aierken712 <ershad0419@gmail.com>
2020-04-10 20:41:08 -0400

    cut it

--- src/test/java/com/bitrix24/pages/Aierken.java
+++ src/test/java/com/bitrix24/pages/Aierken.java
@@ -5,8 +5,7 @@ public class Aierken {
     //Hello Selenium
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.out.println("11111");
-        System.out.println("88888");
+
     }
 
 }
