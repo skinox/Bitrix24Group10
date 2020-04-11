e56d3069c7da4a3d87495d5f72b12d5c64b109f8
Aierken712 <ershad0419@gmail.com>
2020-04-10 20:21:08 -0400

    add number

--- src/test/java/com/bitrix24/pages/Aierken.java
+++ src/test/java/com/bitrix24/pages/Aierken.java
@@ -6,6 +6,7 @@ public class Aierken {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("11111");
+        System.out.println("88888");
     }
 
 }
